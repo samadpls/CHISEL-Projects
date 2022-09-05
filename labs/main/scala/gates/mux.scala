ddf95f1260acdef76aa17e8d0c20c99c462b02ec
samadpls <abdulsamadsid1@gmail.com>
2022-09-05 13:19:37 +0500

    added more lab

---
+++ labs/main/scala/gates/mux.scala
@@ -0,0 +1,27 @@
+package gates
+import chisel3._
+ 
+class mux extends Module{
+    val io=IO(new Bundle{
+        val a=Input(UInt(4.W))
+        val b=Input(UInt(4.W))
+        val sel=Input(UInt(3.W))
+        val x=Output(UInt(4.W))
+
+    })
+    when(io.sel===0.U){
+        io.x:=io.a + io.b
+    }.elsewhen(io.sel===1.U){
+        io.x:=io.a - io.b
+    }.elsewhen(io.sel===2.U){
+        io.x:=io.a & io.b
+    }.elsewhen(io.sel===3.U){
+        io.x:=io.a | io.b
+    }.elsewhen(io.sel===4.U){
+        io.x:=io.a ^ io.b
+    }.elsewhen(io.sel===5.U){
+        io.x:= ~(io.a ^ io.b)
+    }.otherwise{
+        io.x:=0.U
+    }
+}
